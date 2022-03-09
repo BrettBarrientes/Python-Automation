cd47b7bf982ea55e3a46420081a78ef0a6efee29
Brett <B.barrientes25@outlook.com>
2022-03-08 22:22:57 -0500

    Automation Transfer of Files
    Goal of this project was to reduce the time it takes to transfer multiple files from one folder to another. A new directory that follows a year/month is made for weekly files. Suppose to save time from editing that can take 15-20 minutes to simply 30 seconds or less.

---
+++ First_Script_Project.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from pathlib import Path
+import os
+import shutil
+
+start_path = 'C:/Users/Brett/Documents/Python/'
+dest_path = 'C:/Users/Brett/Documents/New-Path-Test/'
+
+old_files = os.listdir(start_path)
+
+#Moving each file under start_path to dest_path
+for old_file in old_files:
+    shutil.move(start_path + old_file, dest_path + old_file)
+
+print(old_file)
+print("Files have been transferred successfully!")
+
+new_dest_path = Path(r"C:\Users\Brett\Documents\New-Path-Test")
+week_num = input("Enter the week of the Month: ")
+
+for file in new_dest_path.iterdir():
+    if file.is_file():
+        directory = file.parent
+        extension = file.suffix
+
+        old_name = file.stem
+        region, report_type, old_date = old_name.split('-')
+
+        old_date = datetime.strptime(old_date, '%Y%b%d')
+        date = datetime.strftime(old_date, '%b')
+
+        ##print(date)
+
+        new_name = f'{region}-{date}-{report_type}-{week_num}{extension}'
+        #print(new_name)
+        year_month = datetime.strftime(old_date, "%Y-%b")
+
+        new_directory = new_dest_path.joinpath(year_month)
+        #print(new_path)
+        #if not os.path.exists(new_name):
+        #if the directory does not exist =>
+            #create a new directory then move the files into the new directory and rename them
+        if not new_directory.exists():
+            new_directory.mkdir(parents=True, exist_ok=True)
+            new_file_path = new_directory.joinpath(new_name)
+            print(new_file_path)
+            file.rename(new_file_path)
+            #print(new_file_path)
+        else:
+            #if the directory does exist =>
+                #move the files into the existing directory and rename them
+            new_file_path = new_directory.joinpath(new_name)
+            print(new_file_path)
+            file.rename(new_file_path)
+            #print(new_file_path)
+
+print('Files have been added and renamed successfully!')
+print(new_file_path)
