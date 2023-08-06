6ca627d0fff67d9e7f0bbb4064078b372452e47b
BrettBarrientes <b.barrientes25@outlook.com>
2023-08-06 15:22:56 -0400

    Updated the file transfer script

--- First_Script_Project.py
+++ First_Script_Project.py
@@ -3,19 +3,20 @@ from pathlib import Path
 import os
 import shutil
 
-start_path = 'C:/Users/Brett/Documents/Python/'
-dest_path = 'C:/Users/Brett/Documents/New-Path-Test/'
+
+start_path = 'C:/Users/Documents/Old_Current_Path/' # Current Directory of the files
+dest_path = 'C:/Users/Documents/New_Path/' # Directory where the files are to be moved
 
 old_files = os.listdir(start_path)
 
-#Moving each file under start_path to dest_path
+# Moving each file under start_path to dest_path
 for old_file in old_files:
     shutil.move(start_path + old_file, dest_path + old_file)
 
 print(old_file)
 print("Files have been transferred successfully!")
 
-new_dest_path = Path(r"C:\Users\Brett\Documents\New-Path-Test")
+new_dest_path = Path(r"C:\Users\Brett\Documents\New_Path")
 week_num = input("Enter the week of the Month: ")
 
 for file in new_dest_path.iterdir():
@@ -37,9 +38,9 @@ for file in new_dest_path.iterdir():
 
         new_directory = new_dest_path.joinpath(year_month)
         #print(new_path)
-        #if not os.path.exists(new_name):
-        #if the directory does not exist =>
-            #create a new directory then move the files into the new directory and rename them
+        # if not os.path.exists(new_name):
+        # if the directory does not exist =>
+            # create a new directory then move the files into the new directory and rename them
         if not new_directory.exists():
             new_directory.mkdir(parents=True, exist_ok=True)
             new_file_path = new_directory.joinpath(new_name)
@@ -47,8 +48,8 @@ for file in new_dest_path.iterdir():
             file.rename(new_file_path)
             #print(new_file_path)
         else:
-            #if the directory does exist =>
-                #move the files into the existing directory and rename them
+            # if the directory does exist =>
+                # move the files into the existing directory and rename them
             new_file_path = new_directory.joinpath(new_name)
             print(new_file_path)
             file.rename(new_file_path)
